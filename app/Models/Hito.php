eddc36446c32bee72c395a8b67e7ad7da01aa5f3
CeciJhoana <ceciliavergara997@gmail.com>
2024-09-20 20:26:10 -0400

    Add hito and objetivo class

--- app/Models/Hito.php
+++ app/Models/Hito.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Hito extends Model
 {
-    use HasFactory;
+    protected $table = 'hito';
+    protected $primaryKey = ['id_proyecto', 'id_hito']; // Llave compuesta
+    public $timestamps = false;
+
+    protected $fillable = ['id_proyecto', 'numero_hito', 'fecha_inicio_hito', 'fecha_fin_hito'];
+
+    // Relación con Proyecto
+    public function proyecto()
+    {
+        return $this->belongsTo(Proyecto::class, 'id_proyecto', 'id_proyecto');
+    }
 }
--- app/Models/Objetivo.php
+++ app/Models/Objetivo.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Objetivo extends Model
 {
-    use HasFactory;
+    protected $table = 'objetivo';
+    protected $primaryKey = 'id_objetivo';
+    public $timestamps = false;
+
+    protected $fillable = ['id_proyecto', 'id_hito', 'descrip_objetivo', 'prioridad', 'fecha_ini_objetivo', 'fecha_fin_objetivo'];
+
+    // Relación con Hito
+    public function hito()
+    {
+        return $this->belongsTo(Hito::class, 'id_hito', 'id_hito');
+    }
 }
