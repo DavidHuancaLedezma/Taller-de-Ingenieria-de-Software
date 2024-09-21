848520fa0e4ea6ac1c2f13822564f70c0cc2eabc
CeciJhoana <ceciliavergara997@gmail.com>
2024-09-20 20:22:12 -0400

    Add grupo and proyecto class

--- app/Models/Grupo.php
+++ app/Models/Grupo.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Grupo extends Model
 {
-    use HasFactory;
+    protected $table = 'grupo';
+    protected $primaryKey = 'id_grupo';
+    public $timestamps = false;
+
+    protected $fillable = ['nombre_grupo'];
+
+    // Relación con Proyecto
+    public function proyectos()
+    {
+        return $this->hasMany(Proyecto::class, 'id_grupo', 'id_grupo');
+    }
 }
