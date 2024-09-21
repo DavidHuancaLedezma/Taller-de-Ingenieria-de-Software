8b77624fbd97d1c352a38c4419cb185235de625c
CeciJhoana <ceciliavergara997@gmail.com>
2024-09-20 20:20:26 -0400

    Add estudiante grupo empresas class

--- app/Models/EstudianteGrupoEmpresa.php
+++ app/Models/EstudianteGrupoEmpresa.php
@@ -2,10 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class EstudianteGrupoEmpresa extends Model
 {
-    use HasFactory;
+    protected $table = 'estudiante_grupoempresa';
+    public $timestamps = false;
+    protected $primaryKey = ['id_usuario', 'id_grupo_empresa']; // Llave compuesta
+
+    protected $fillable = ['id_usuario', 'id_grupo_empresa', 'periodo_grupoempresa'];
+
+    // Relación con Usuario
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class, 'id_usuario', 'id_usuario');
+    }
+
+    // Relación con GrupoEmpresa
+    public function grupoEmpresa()
+    {
+        return $this->belongsTo(GrupoEmpresa::class, 'id_grupo_empresa', 'id_grupo_empresa');
+    }
 }
--- app/Models/GrupoEmpresa.php
+++ app/Models/GrupoEmpresa.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class GrupoEmpresa extends Model
 {
-    use HasFactory;
+    protected $table = 'grupo_empresa';
+    protected $primaryKey = 'id_grupo_empresa';
+    public $timestamps = false;
+
+    protected $fillable = ['codigo_acceso', 'nombre_largo', 'nombre_corto', 'direccion', 'correo_eletronico_ge', 'telefono_ge'];
+
+    // Relación con Proyecto
+    public function proyectos()
+    {
+        return $this->hasMany(Proyecto::class, 'id_grupo_empresa', 'id_grupo_empresa');
+    }
 }
