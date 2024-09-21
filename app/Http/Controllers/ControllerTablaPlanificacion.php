beee2cdd529c10e0b05a3c872dba2ab14821647d
CeciJhoana <ceciliavergara997@gmail.com>
2024-09-20 20:08:00 -0400

    Fix: controller tabla planificacion

--- app/Http/Controllers/ControllerTablaPlanificacion.php
+++ app/Http/Controllers/ControllerTablaPlanificacion.php
@@ -9,8 +9,8 @@ class ControllerTablaPlanificacion extends Controller
 {
     public function getTabla()
     {
-        // cambiar el id del proyecto, esta estico ahora y no dinamico para hacer la consulta
-        //$datos = DB::select("SELECT h.id_hito, h.numero_hito, h.fecha_inicio_hito, h.fecha_fin_hito FROM hito h, proyecto pr WHERE h.id_proyecto = pr.id_proyecto AND pr.id_proyecto = 1");
-        //return view('tablaPlanificacion', ['hitos' => $datos]);
+        //cambiar el id del proyecto, esta estico ahora y no dinamico para hacer la consulta
+        $datos = DB::select("SELECT h.id_hito, h.numero_hito, h.fecha_inicio_hito, h.fecha_fin_hito FROM hito h, proyecto pr WHERE h.id_proyecto = pr.id_proyecto AND pr.id_proyecto = 1");
+        return view('tablaPlanificacion', ['hitos' => $datos]);
     }
 }
