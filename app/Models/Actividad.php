9b10454dcb75d6c75a76bc53331b4d7027acff99
CeciJhoana <ceciliavergara997@gmail.com>
2024-09-20 20:29:24 -0400

    Add actividad and criterio de aceptacion

--- app/Models/Actividad.php
+++ app/Models/Actividad.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Actividad extends Model
 {
-    use HasFactory;
+    protected $table = 'actividad';
+    protected $primaryKey = ['id_usuario', 'id_objetivo', 'id_actividad']; // Llave compuesta
+    public $timestamps = false;
+
+    protected $fillable = ['id_usuario', 'id_objetivo', 'descripcion_actividad', 'resultado', 'realizado'];
+
+    // Relación con Usuario
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class, 'id_usuario', 'id_usuario');
+    }
 }
--- app/Models/CriterioAceptacion.php
+++ app/Models/CriterioAceptacion.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CriterioAceptacion extends Model
 {
-    use HasFactory;
+    protected $table = 'criterio_aceptacion';
+    protected $primaryKey = ['id_objetivo', 'id_criterio_aceptacion']; // Llave compuesta
+    public $timestamps = false;
+
+    protected $fillable = ['id_objetivo', 'descripcion_ca', 'cumplido'];
+
+    // Relación con Objetivo
+    public function objetivo()
+    {
+        return $this->belongsTo(Objetivo::class, 'id_objetivo', 'id_objetivo');
+    }
 }
