633a35911bf1185fc5bc491c432319175ef14106
Erika Chino <erika.53chinoalanoca@gmail.com>
2024-09-21 21:17:52 -0400

    feat: Agregando archivo de estilos css

--- resources/views/grupo_empresa/registroGE.blade.php
+++ resources/views/grupo_empresa/registroGE.blade.php
@@ -8,7 +8,7 @@
     <title>REGISTRO GRUPO EMPRESA</title>
 
     <!-- CSS -->
-
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
 
